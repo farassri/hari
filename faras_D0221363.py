77fb089da62ff8cfc888cb1d16f0918f4d1cd4ce
A C E R <nhunu3864@gmail.com>
2022-03-14 13:00:17 +0800

    pertama

---
+++ faras_D0221363.py
@@ -0,0 +1,25 @@
+#string
+buah=("apel")
+print(buah)
+#float
+satu=13,2
+print(satu)
+#integer
+a=11
+print(a)
+#boolean
+angka1=7>1
+print(angka1)
+angka2=7<1
+print(angka2)
+
+#contoh aritmetika
+nilai1=10
+nilai2=8
+print(nilai1+nilai2)
+print(nilai1-nilai2)
+print(nilai1*nilai2)
+print(nilai1/nilai2)
+print(nilai1%nilai2)
+print(nilai1**nilai2)
+print(nilai1//nilai2)
